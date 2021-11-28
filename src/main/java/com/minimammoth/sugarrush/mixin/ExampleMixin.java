4b1f95bb0034431c26c3d640bbd2e453a22a2b67
Raphael Zöllner <raphael.zoellner@outlook.com>
2021-11-28 19:11:42 +0100

    Add initial files

---
+++ src/main/java/com/minimammoth/sugarrush/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package com.minimammoth.sugarrush.mixin;
+
+import com.minimammoth.sugarrush.SugarRush;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		SugarRush.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
